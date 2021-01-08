f6f0490e68f826eccde977d7d6e0766c67b01325
LOuroboros <lunosouroboros@gmail.com>
2021-01-08 20:39:42 -0300

    Added a special to change a Deoxys' form And an original event object script as an example of its usage.

--- src/field_specials.c
+++ src/field_specials.c
@@ -67,6 +67,7 @@
 #include "constants/weather.h"
 #include "constants/metatile_labels.h"
 #include "palette.h"
+#include "constants/metatile_behaviors.h"
 
 EWRAM_DATA bool8 gBikeCyclingChallenge = FALSE;
 EWRAM_DATA u8 gBikeCollisions = 0;
@@ -4364,4 +4365,54 @@ void SetPlayerGotFirstFans(void)
 u8 Script_TryGainNewFanFromCounter(void)
 {
     return TryGainNewFanFromCounter(gSpecialVar_0x8004);
+}
+
+// Changes a Deoxys' form if the following conditions are met:
+// -gSpecialVar_0x8004 is currently hosting a Deoxys form.
+// -The metatile behavior of the tile in front of the Player is MB_UNUSED_2C, MB_UNUSED_2D, MB_UNUSED_2E or MB_UNUSED_2F.
+// If these conditions aren't met, gSpecialVar_Result is set to FALSE meaning Deoxys' form didn't change.
+bool16 TryChangeDeoxysForm(void)
+{
+#ifdef POKEMON_EXPANSION
+    u16 baseSpecies = GetMonData(&gPlayerParty[gSpecialVar_0x8004], MON_DATA_SPECIES);
+    u16 targetSpecies;
+    u8 metatileBehavior;
+
+    if (baseSpecies == SPECIES_DEOXYS
+     || baseSpecies == SPECIES_DEOXYS_ATTACK
+     || baseSpecies == SPECIES_DEOXYS_DEFENSE
+     || baseSpecies == SPECIES_DEOXYS_SPEED)
+    {
+        struct MapPosition position;
+        extern struct MapPosition gPlayerFacingPosition;
+        GetXYCoordsOneStepInFrontOfPlayer(&gPlayerFacingPosition.x, &gPlayerFacingPosition.y);
+        metatileBehavior = MapGridGetMetatileBehaviorAt(gPlayerFacingPosition.x, gPlayerFacingPosition.y);
+
+        switch (metatileBehavior)
+        {
+            case MB_UNUSED_2C:
+                targetSpecies = SPECIES_DEOXYS;
+                break;
+            case MB_UNUSED_2D:
+                targetSpecies = SPECIES_DEOXYS_ATTACK;
+                break;
+            case MB_UNUSED_2E:
+                targetSpecies = SPECIES_DEOXYS_DEFENSE;
+                break;
+            case MB_UNUSED_2F:
+                targetSpecies = SPECIES_DEOXYS_SPEED;
+                break;
+            default:
+                gSpecialVar_Result = FALSE;
+                return;
+        }
+
+        SetMonData(&gPlayerParty[gSpecialVar_0x8004], MON_DATA_SPECIES, &targetSpecies);
+        CalculateMonStats(&gPlayerParty[gSpecialVar_0x8004]);
+        gSpecialVar_Result = TRUE;
+        return;
+    }
+
+    gSpecialVar_Result = FALSE;
+#endif
 }
