90d99655fcb31f1bf53d7cb9fd9d97fabbcaf1c1
Midnight <mails.midnight@gmail.com>
2024-08-03 10:13:07 +0800

    Various small updates

--- include/battle_setup.h
+++ include/battle_setup.h
@@ -67,6 +67,7 @@ bool8 ShouldTryRematchBattle(void);
 bool8 IsTrainerReadyForRematch(void);
 void ShouldTryGetTrainerScript(void);
 u16 CountBattledRematchTeams(u16 trainerId);
+void LevelCapToString(void);
 
 void DoStandardWildBattle_Debug(void);
 void BattleSetup_StartTrainerBattle_Debug(void);
--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,9 +43,11 @@
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
 #define FLAG_DISABLE_BAG    0x20 // Unused Flag
+#define FLAG_DISABLE_BAG     0x20 // Unused Flag
 #define FLAG_NO_ENCOUNTER    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
 #define FLAG_UNUSED_0x023    0x23 // Unused Flag
+#define FLAG_EXP_SHARE       0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
