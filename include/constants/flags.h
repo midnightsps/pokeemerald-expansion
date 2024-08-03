b58cd667104b303b75e6c0e251f1e35cd3b50afd
Midnight <mails.midnight@gmail.com>
2024-08-03 10:30:22 +0800

    Minor cleanup

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -42,11 +42,9 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_DISABLE_BAG    0x20 // Unused Flag
 #define FLAG_DISABLE_BAG     0x20 // Unused Flag
 #define FLAG_NO_ENCOUNTER    0x21 // Unused Flag
 #define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
 #define FLAG_EXP_SHARE       0x23 // Unused Flag
 #define FLAG_UNUSED_0x024    0x24 // Unused Flag
 #define FLAG_UNUSED_0x025    0x25 // Unused Flag
