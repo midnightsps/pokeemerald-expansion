fa97791efad826ede63b87079ca2d55f66e3c205
Midnight <mails.midnight@gmail.com>
2024-08-03 08:12:39 +0800

    Update level_caps.h

--- include/config/level_caps.h
+++ include/config/level_caps.h
@@ -16,6 +16,6 @@
 #define B_LEVEL_CAP_VARIABLE            0 // event variable used to derive level cap if B_LEVEL_CAP_TYPE is set to LEVEL_CAP_VARIABLE
 
 #define B_RARE_CANDY_CAP                FALSE // If set to true, Rare Candies can't be used to go over the level cap
-#define B_LEVEL_CAP_EXP_UP              FALSE // If set to true, mons under level cap will receive more experience
+#define B_LEVEL_CAP_EXP_UP              TRUE // If set to true, mons under level cap will receive more experience
 
 #endif /* GUARD_CONFIG_LEVEL_CAP_H */
