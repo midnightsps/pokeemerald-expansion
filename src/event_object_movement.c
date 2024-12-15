3c75b0e78469e65b733ad98d288334fca02e5095
Midnight <mails.midnight@gmail.com>
2024-12-16 01:59:29 +0800

    Allow Jumping Over Ledges with Acro Bike

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -53,6 +53,8 @@
 #include "constants/trainer_types.h"
 #include "constants/union_room.h"
 #include "constants/weather.h"
+#include "constants/metatile_behaviors.h"
+#include "bike.h"
 
 // this file was known as evobjmv.c in Game Freak's original source
 
@@ -9548,6 +9550,7 @@ u8 GetLedgeJumpDirection(s16 x, s16 y, u8 direction)
 
     u8 behavior;
     u8 index = direction;
+    struct ObjectEvent *playerObjEvent = &gObjectEvents[gPlayerAvatar.objectEventId];
 
     if (index == DIR_NONE)
         return DIR_NONE;
@@ -9559,6 +9562,14 @@ u8 GetLedgeJumpDirection(s16 x, s16 y, u8 direction)
 
     if (ledgeBehaviorFuncs[index](behavior) == TRUE)
         return index + 1;
+
+    if (gPlayerAvatar.acroBikeState == ACRO_STATE_BUNNY_HOP &&
+       MB_JUMP_EAST <= behavior && behavior <= MB_JUMP_SOUTH)
+   {
+       MoveCoords(direction, &x, &y);
+       if (GetCollisionAtCoords(playerObjEvent, x, y, direction) == COLLISION_NONE)
+           return index + 1;
+   }
 
     return DIR_NONE;
 }
